9e56167fa55a09f405c26fae9e080e16c5bae0c7
skyleaworlder <870033938@qq.com>
2023-02-27 08:47:46 +0000

    fix: pr id and specify repo full name

--- src/index.ts
+++ src/index.ts
@@ -2,8 +2,9 @@ import { Probot } from "probot";
 
 export = (app: Probot) => {
   app.on(["issue_comment.edited"], async (context) => {
-    app.log.info("trigger");
+    app.log.info("issue_comment edited");
     if (context.payload.comment.body.includes("@skyleaworlder-test-bot /")) {
+      app.log.info(`inputs: repository: ${context.payload.repository.full_name}`)
       await context.octokit.actions.createWorkflowDispatch({
         owner: "Eternal-Night-Archer",
         repo: "BenchmarkData",
@@ -11,7 +12,8 @@ export = (app: Probot) => {
         ref: "master",
         inputs: {
           "url": context.payload.issue.html_url,
-          "pr_id": context.payload.issue.id.toString()
+          "pr_id": context.payload.issue.number.toString(),
+          "repository": `${context.payload.repository.full_name}`
         }
       });
     } else {
